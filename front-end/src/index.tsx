c78f5417b50994015b64a3eefa5fceed0f173826
Shane Sweeney <shanep.sween@gmail.com>
2021-02-23 09:37:37 -0800

    Fix linting for front end

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { ThemeProvider } from '@material-ui/core/styles'
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import theme from './theme'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={theme} >
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />{' '}
-    <BrowserRouter>
+    <CssBaseline /> {' '}
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </ThemeProvider>,
