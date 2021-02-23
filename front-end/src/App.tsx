ca28bcd6ba9949df51b61f7a6cd92ec07c5fbf5c
Shane Sweeney <shanep.sween@gmail.com>
2021-02-23 11:26:46 -0800

    linter clean

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Button, Paper, Container, Typography } from '@material-ui/core'
-import logo from './logo.svg'
-import Navbar from './Components/navbar'
-import Routes from './routes'
+import React from "react";
+import Navbar from "./Components/navbar";
+import Routes from "./routes";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div>
       <Navbar />
       <Routes />
     </div>
-  )
+  );
 }
-export default App
+export default App;
